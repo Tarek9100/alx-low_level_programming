0bf0d52b02c247a5ab27e446b56ffee960e247ea
Tarek9100 <tareki9100@gmail.com>
2023-06-15 06:13:26 -0700

    Print fn

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints a line using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
