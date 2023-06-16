3e014e59e75b2d6ca54ac09e966e887ba892e3a0
Tarek9100 <tareki9100@gmail.com>
2023-06-16 10:02:00 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i > j || j > k || i > k)
+					continue;
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (i == 7 && j == 8 && k == 9)
+					putchar('\n');
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
