7270d01fda0424b529b3870bc1a6cd25a703172c
Tarek9100 <tareki9100@gmail.com>
2023-06-16 10:39:44 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int i, j, x, y, num1, num2;
 
-	for (x = 0; x < 9; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 9; y++)
+		for (y = 0; y < 10; y++)
 		{
-			for (i = 0; i < 9; i++)
+			for (i = 0; i < 10; i++)
 			{
-				for (j = 0; j < 9; j++)
+				for (j = 0; j < 10; j++)
 				{
 					num1 = i * 10 + j;
 					num2 = x * 10 + y;
