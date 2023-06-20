4d59e36ae3d5c0c226d6c8631f95aa60775d1738
Tarek9100 <tareki9100@gmail.com>
2023-06-20 04:14:17 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,11 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-		_putchar(h);
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
 		_putchar(':');
-		_putchar(m);
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
 		_putchar('\n');
 		}
 	}
