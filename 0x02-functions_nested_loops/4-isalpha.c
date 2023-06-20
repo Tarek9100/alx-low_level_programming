16be21e0585b64f50b95089758d5285e07594dff
Tarek9100 <tareki9100@gmail.com>
2023-06-20 03:03:39 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
+ */
+int _isalpha(int c)
+{
+	if ((c < 123 && c > 96) || (c < 91 && c > 64))
+	{
+		return (1);
+	}
+	return (0);
+}
