f693bbba8993c2ea37b87309699ba30a9be2725d
Tarek9100 <tareki9100@gmail.com>
2023-06-20 02:30:25 -0700

    o

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "Task2.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
