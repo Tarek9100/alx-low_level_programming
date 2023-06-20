e65a6a61c839b62eeae8338d1616f6b6a1c0e52a
Tarek9100 <tareki9100@gmail.com>
2023-06-20 04:30:35 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,15 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, result = 0;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar((i * j) + '0');
+			result = i * j;
+			_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
