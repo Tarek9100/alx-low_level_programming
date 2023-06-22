1ca00e1a6b182712f465f154925a56d08f3eb0c5
Tarek9100 <tareki9100@gmail.com>
2023-06-22 02:31:54 -0700

    u

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,11 @@
 *@c: tested character
 *Return: returns 1 if it is uppercase , 0 if not
 */
+
 int _isdigit(int c)
 {
-	if (c >= 0 || c <= 9)
-	{
-		return (1);
-	}
-	return (0);
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+_putchar('\n');
+}
+}
