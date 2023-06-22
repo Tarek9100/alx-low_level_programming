64c03121bc1203c40a395b8a077cbf949e091c7a
Tarek9100 <tareki9100@gmail.com>
2023-06-22 03:45:02 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,10 +16,12 @@ printf("FizzBuzz ");
 else if ((i % 3) == 0)
 printf("Fizz ");
 else if ((i % 5) == 0)
+if (i != 100)
 printf("Buzz ");
+else
+printf("Buzz\n");
 else
 printf("%d ", i);
 }
-printf("\n");
 return (0);
 }
