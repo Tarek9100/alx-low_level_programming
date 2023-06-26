0f8dada9861d6d6a51ec2d87ba80c3ef950cf271
Tarek9100 <tareki9100@gmail.com>
2023-06-26 13:25:03 -0700

    H

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,13 @@
 void puts_half(char *str)
 {
 int i = 0, h;
-while (s[i] != '\0')
+while (str[i] != '\0')
 {
 i++;
 }
 h = i / 2;
-for (; s[h] != '\0'; h++)
+for (; str[h] != '\0'; h++)
 {
-_putchar(s[h]);
+_putchar(str[h]);
 }
 }
