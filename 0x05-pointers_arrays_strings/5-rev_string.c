0cf87bee72f8d7f2deb1a7a1941541141668a9b3
Tarek9100 <tareki9100@gmail.com>
2023-06-26 13:24:01 -0700

    k

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * rev_string - This fuction print a string reverser
@@ -7,15 +8,24 @@
 
 void rev_string(char *s)
 {
-int i = 0, j = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-char m[i] = {0};
-for (; s[i] != '\0'; i--)
-{
-m[j] = s[i];
-j++;
-}
+	int a, len;
+
+	char *begin, *end = s;
+
+	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+	{
+		end++;
+	}
+	len = a + 1;
+	begin = s;
+	for (a = 0; a < len / 2; a++)
+	{
+		char x;
+		x = *end;
+		*end = *begin;
+		*begin = x;
+		begin++;
+		end--;
+	}
+	end[len + 1] = '\0';
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts_half - function that prints half of a string, followed by a new line.
+ *
+ *@str: This is the input string
+ */
+void puts_half(char *str)
+{
+int i = 0, h;
+while (s[i] != '\0')
+{
+i++;
+}
+h = i / 2;
+for (; s[h] != '\0'; h++)
+{
+_putchar(s[h]);
+}
+}
