8c8fa392ffbbba22e9be1510d5d5a76ab094c801
Tarek9100 <tareki9100@gmail.com>
2023-06-29 09:39:01 -0700

    u

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - Write a function that encodes a string into 1337
+ *
+ * @changed: This is the input string
+ *
+ * Return: String converted to 1337
+ */
+
+char *leet(char *changed)
+{
+int i, j;
+char low[] = {o, e, a, t, l};
+char high[] = {O, E, A, T, L};
+char num[] = {0, 3, 4, 7, 1};
+
+for (i = 0; changed[i] != '\0'; i++)
+{
+for (j = 0; j < 5; j++)
+{
+if (changed[i] == low[j] || changed[i] == high[j])
+{
+changed[i] = num[j];
+}
+}
+}
+return (changed);
+}
